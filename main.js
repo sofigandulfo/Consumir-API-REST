e1a63895898d7223140cb9784809b85cfd74fbbf
sofigandulfo <sofigandulfo@gmail.com>
2023-05-21 21:33:26 -0300

    Se agregó función para volver a la imagen anterior(solo 1) y pasar a la actual imagen. Botones correspondientes a cada accion

--- main.js
+++ main.js
@@ -1,19 +1,59 @@
+//API REST
 const URL = 'https://api.thecatapi.com/v1/images/search'
+let listaUrl = [];
+let actualUrl = '';
 
+
+
+//Petición (?)
 const gatitos = async () => {
     const res = await fetch(URL);
     const data = await res.json();
-    const img = document.querySelector('img');
+    const url = data[0].url;
+    const img = document.querySelector('#img');
         img.src = data[0].url;
-    
+    actualUrl = url;
+    listaUrl.push(actualUrl);
+
+
     return img;
 }
 gatitos();
 
 
-const boton = document.querySelector('button');
+//Función para volver a la imagen anterior (solo vuelve al último)
+const cargarImagenAnterior = () => {
+    let copiaListaUrl = listaUrl.slice();
+    const ultimaImg = copiaListaUrl[copiaListaUrl.length-2];
+    const img = document.querySelector('#img');
+    img.src = ultimaImg;
+}
 
+
+//Función para volver a la imagen siguiente
+const cargarSiguiente = () => {
+    let copiaListaUrl = listaUrl.slice();
+    const siguiente = copiaListaUrl[copiaListaUrl.length-1];
+    const img = document.querySelector('#img');
+    img.src = siguiente;
+}
+
+
+//Botón para generar una imagen random
+const boton = document.querySelector('button');
+//Evento del boton
 boton.addEventListener('click', gatitos);
+
+
+//Botón para volver a la imagen anterior
+const back = document.getElementById('back');
+//Evento del boton
+back.addEventListener('click', cargarImagenAnterior);
+
+//Botón para ir a la imagen siguiente
+const siguiente = document.getElementById('siguiente');
+//Evento del boton
+siguiente.addEventListener('click', cargarSiguiente);
 
 
 /*
@@ -29,7 +69,4 @@ function consumirAPI(){
 
 const boton = document.querySelector('button');
 
-boton.addEventListener('click', consumirAPI);
-
-
- */
+boton.addEventListener('click', consumirAPI);*/
